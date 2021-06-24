ec93165beb81b430bd0055bf1f9a19de6b7d5978
RamadhanZain <rbuma8426@gmail.com>
2021-06-24 13:51:25 +0700

    firebase_test

--- firebase_test.py
+++ firebase_test.py
@@ -1,21 +1,30 @@
 from firebase import firebase
 from random import randint
+import time
 
 url = 'https://dynamic-traffic-light-default-rtdb.firebaseio.com'
 firebase = firebase.FirebaseApplication(url, None)
 
-data = {
-    "duration": randint(0, 20)
-}
+
 
 
 
 while True:
+    selatan=randint(0,20)
+    barat=randint(0,20)
+    timur=randint(0,20)
+    utara=randint(0,20)
+    dct_car={
+    'selatan':selatan,
+    'barat':barat,
+    'timur':timur,
+    'utara':utara
+    }
     # result = firebase.patch('/Duration/ruas-barat/', {"duration": randint(0, 20)})
-    result1 = firebase.patch('/Duration/ruas-barat/', {"duration": randint(0, 20)})
-    result2 = firebase.patch('/Duration/ruas-selatan/', {"duration": randint(0, 20)})
-    result3 = firebase.patch('/Duration/ruas-timur/', {"duration": randint(0, 20)})
-    result4 = firebase.patch('/Duration/ruas-utara/', {"duration": randint(0, 20)})
+    result = firebase.patch('/Vehicle/ruas/', dct_car)
+    # result1 = firebase.patch('/Vehicle/ruas-barat/', data["barat"])
+    # result3 = firebase.patch('/Vehicle/ruas-timur/', data["timur"])
+    # result4 = firebase.patch('/Vehicle/ruas-utara/', data["utara"])
     # result1 = firebase.put('/dynamic-traffic-light-default-rtdb/Duration/ruas-barat', 'duration', randint(0, 20))
     # result2 = firebase.put('/dynamic-traffic-light-default-rtdb/Duration/ruas-selatan', 'duration', randint(0, 20))
     # result3 = firebase.put('/dynamic-traffic-light-default-rtdb/Duration/ruas-timur', 'duration', randint(0, 20))
@@ -29,9 +38,8 @@ while True:
     # print(result["ruas-timur"]["duration"])
     # print(result1 + " " + result2 + " " + result2 + " " + result4)
     # print(str(result1["duration"]) + " " + str(result2["duration"]) + " " + str(result2["duration"]) + " " + str(result4["duration"]))
-    lst = [result1["duration"], result2["duration"], result3["duration"], result4["duration"]]
     # print(result)
     # print(result1)
-    print(lst)
+    print(result.values())
 
     # {'1': 'John Doe', '2': 'Jane Doe'}
